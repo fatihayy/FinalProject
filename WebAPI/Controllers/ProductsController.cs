d5fd5b13391d142bb5ea4b82e3701a19ad0851be
fatih <fatih@LAPTOP-I11R2PMT>
2022-08-17 16:26:25 +0300

    17.08 ilk commit

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -29,7 +29,7 @@ namespace WebAPI.Controllers
             //Swagger
             //Dependency Chain -- 
 
-            Thread.Sleep(1000);
+            //Thread.Sleep(1000);
 
             var result = _productService.GetAll();
             if (result.Success)
